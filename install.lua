e97f232563d932bde505c89de350e58739be6caa
Julius <clashzocker2008@gmail.com>
2025-01-06 20:23:23 +0100

    own Fork

--- install.lua
+++ install.lua
@@ -2,8 +2,8 @@
 --
 --
 
-local libURL = "https://raw.githubusercontent.com/coaber/DEReactor/master/lib/f.lua"
-local startupURL = "https://raw.githubusercontent.com/coaber/DEReactor/master/drmon.lua"
+local libURL = "https://raw.githubusercontent.com/JFK20/DEReactor/master/lib/f.lua"
+local startupURL = "https://raw.githubusercontent.com/JFK20/DEReactor/master/drmon.lua"
 local lib, startup
 local libFile, startupFile
 
